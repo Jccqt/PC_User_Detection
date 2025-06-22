b3fecd6ba9df5d43153aed505e4432ac4a81377e
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-06-22 21:49:02 +0800

    Feat (Camera): User can now capture images using the webcam

--- PCUserDetection/UserFaceDetector.Designer.cs
+++ PCUserDetection/UserFaceDetector.Designer.cs
@@ -31,6 +31,7 @@
             this.cbCamera = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pbCamera = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,9 +41,9 @@
             this.cbCamera.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.cbCamera.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbCamera.FormattingEnabled = true;
-            this.cbCamera.Location = new System.Drawing.Point(160, 23);
+            this.cbCamera.Location = new System.Drawing.Point(175, 23);
             this.cbCamera.Name = "cbCamera";
-            this.cbCamera.Size = new System.Drawing.Size(251, 36);
+            this.cbCamera.Size = new System.Drawing.Size(236, 36);
             this.cbCamera.TabIndex = 0;
             // 
             // label1
@@ -57,23 +58,36 @@
             // 
             // pbCamera
             // 
-            this.pbCamera.Location = new System.Drawing.Point(76, 113);
+            this.pbCamera.Location = new System.Drawing.Point(76, 96);
             this.pbCamera.Name = "pbCamera";
             this.pbCamera.Size = new System.Drawing.Size(543, 350);
             this.pbCamera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCamera.TabIndex = 2;
             this.pbCamera.TabStop = false;
             // 
+            // btnCapture
+            // 
+            this.btnCapture.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapture.Location = new System.Drawing.Point(289, 472);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(113, 36);
+            this.btnCapture.TabIndex = 3;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
             // UserFaceDetector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 511);
+            this.ClientSize = new System.Drawing.Size(842, 535);
+            this.Controls.Add(this.btnCapture);
             this.Controls.Add(this.pbCamera);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbCamera);
             this.Name = "UserFaceDetector";
             this.Text = "PC User Face Detector";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserFaceDetector_FormClosing);
             this.Load += new System.EventHandler(this.UserFaceDetector_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).EndInit();
             this.ResumeLayout(false);
@@ -86,6 +100,7 @@
         private System.Windows.Forms.ComboBox cbCamera;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pbCamera;
+        private System.Windows.Forms.Button btnCapture;
     }
 }
 
