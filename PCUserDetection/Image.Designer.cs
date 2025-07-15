324296e1cf3a5896b2dd0a7461cea31874f96a32
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-07-15 08:39:33 +0800

    Feat (Images): System can now display captured images on Images page

---
+++ PCUserDetection/Image.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace PCUserDetection
+{
+    partial class Image
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(116, 0);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(34, 22);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "X";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(0, 0);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(150, 150);
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // Image
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.pbImage);
+            this.Name = "Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnDelete;
+        public System.Windows.Forms.PictureBox pbImage;
+    }
+}
--- PCUserDetection/UserFaceDetector.Designer.cs
+++ PCUserDetection/UserFaceDetector.Designer.cs
@@ -36,6 +36,7 @@
             this.btnAddUser = new System.Windows.Forms.Button();
             this.lblAlert = new System.Windows.Forms.Label();
             this.btnSetEmail = new System.Windows.Forms.Button();
+            this.btnImages = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).BeginInit();
             this.SuspendLayout();
             // 
@@ -126,11 +127,23 @@
             this.btnSetEmail.UseVisualStyleBackColor = true;
             this.btnSetEmail.Click += new System.EventHandler(this.btnSetEmail_Click);
             // 
+            // btnImages
+            // 
+            this.btnImages.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImages.Location = new System.Drawing.Point(694, 198);
+            this.btnImages.Name = "btnImages";
+            this.btnImages.Size = new System.Drawing.Size(113, 36);
+            this.btnImages.TabIndex = 8;
+            this.btnImages.Text = "Images";
+            this.btnImages.UseVisualStyleBackColor = true;
+            this.btnImages.Click += new System.EventHandler(this.btnImages_Click);
+            // 
             // UserFaceDetector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(842, 535);
+            this.Controls.Add(this.btnImages);
             this.Controls.Add(this.btnSetEmail);
             this.Controls.Add(this.btnAddUser);
             this.Controls.Add(this.lblAlert);
@@ -160,6 +173,7 @@
         private System.Windows.Forms.Button btnAddUser;
         private System.Windows.Forms.Label lblAlert;
         private System.Windows.Forms.Button btnSetEmail;
+        private System.Windows.Forms.Button btnImages;
     }
 }
 
